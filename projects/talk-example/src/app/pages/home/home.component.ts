9bc52e68bf70c493ff7c544a8f51732ba53a9842
iannonss <iannonemattia7@gmai.com>
2024-08-23 17:54:15 +0200

    added home

---
+++ projects/talk-example/src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <div></div>`,
+  styles: ``
+})
+export class HomeComponent {
+
+}
