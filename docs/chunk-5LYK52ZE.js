696bf9747aad4c00d8c0f6375ac1fa49a3b323c8
c.ricciardi <chiara.ricciardi66.cr@gmail.com>
2024-09-16 15:50:05 +0200

    config for git pages

---
+++ docs/chunk-5LYK52ZE.js
@@ -0,0 +1,1 @@
+var a={production:!1,apiUrl:"https://api.nasa.gov",apiImageUrl:"https://images-api.nasa.gov",apiKey:"WD6jXxpF1Lv8GB8UXxyrfp1oG3fIYOtG4auvEr3C"};export{a};
--- projects/images-at-light-speed-unoptimized-old/src/app/pages/solar-system/models/PlanetComponentModel.ts
+++
@@ -1,9 +0,0 @@
-export class PlanetComponentModel {
-  planet: string;
-  componentCallback: string;
-
-  constructor(planet: string, componentCallback: string) {
-    this.planet = planet;
-    this.componentCallback = componentCallback;
-  }
-}
